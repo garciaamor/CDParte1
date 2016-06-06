f609ff972c2945fd26383b6a10cd3d2ebe16527e
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-06-06 09:19:53 +0200

    refactorizada clase Principal

--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,47 +1,33 @@
 package examenfinalparte1;
 
+import java.io.IOException;
+
 /**
- *  Clase con los metodos que vamos a llamar en la clase main y el metodo base
- * Las tres clases se llaman Principal, pero se diferencian en el numero de variables que reciben
+ * Clase Principal del proyecto, que contiene el main
  * @author jgarciaamor
  */
 public class Principal {
 
-    int i;
-
-    /**
-     * Metodo que recibe dos variables de tipo int, llamadas j y m, y cambia el valor de i; no devuelve nada
-     * Le asigna a i la operacion met con j y m, es decir, la suma de las dos variables
-     * @param j Primera variable de tipo int que recibe el metodo
-     * @param m Segunda variable de tipo int que recibe el metodo
-     */
-    public Principal(int j, int m) {
-        i = met(j, m);
-    }
-    /**
-    * Metodo que recibe una variable de tipo int, j y cambia el valor de i; no devuelve nada 
-    * Le asigna a i la operacion met con j dos veces, es decir, suma la misma variable consigo misma
-    * @param j Unica variable de tipo int que recibe el metodo y que se sumara dos veces
-    */
-    public Principal(int j) {
-        i = met(j, j);
-    }
-/**
- * Metodo que no recibe ninguna variable y no devuelve nada, lo unico que hace es asignarle a la variable i el valor -1
- */
-    public Principal() {
-        i = -1;
-    }
-/**
- * Metodo que recibe dos variables (x y x1) y nos devuelve un tipo int con la suma de los dos
- * Va a ser llamado en el resto de metodos para realizar las operaciones
- * @param x Variable de tipo int que recibe el metodo
- * @param x1 Variable de tipo int que recibe el metodo
- * @return El metodo nos devuelve un tipo int con la suma de las dos variables
- */
-    public int met(int x, int x1) {
-        return x + x1;
+    public static void main(String[] args) throws IOException {
+        /**
+         * Crea un objeto de tipo ClaseOperacion, llamado sum2v, que recibe dos variables (10 y 20)
+         * Lo que nos indica que llama al constructor que realiza la suma con dos variables distintas
+         */
+        ClaseOperacion sum2v = new ClaseOperacion(10, 20);
+        /**
+         * 
+         */
+        int sum2 = sum2v.result;
+        System.out.println("Es: " + sum2);
+        
+        
+        ClaseOperacion nm = new ClaseOperacion(10);
+        int m = nm.result;
+        System.out.println("Es: " + m);
+        
+        
+        ClaseOperacion mn = new ClaseOperacion();
+        int nn = mn.result;
+        System.out.println("Es: " + nn);
     }
 }
-
-
